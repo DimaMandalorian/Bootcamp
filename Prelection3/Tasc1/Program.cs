ca764cf3e21589a3b0a61345b5a8ad9488d1161a
Dmitriy <dmitriy@MacBook-Pro-Dmitriy.local>
2023-02-23 02:01:16 +0500

    Время на поиск элемента О(1)

---
+++ Prelection3/Tasc1/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = new int[5];
+for (int i = 0; i < 5; i++)
+    array [i] = Convert.ToInt32(Console.ReadLine()!);// заполняем числами
+//Console.WriteLine($"[{string.Join(", ", array)}]");
+Console.WriteLine("[" + string.Join(", ", array) + "]");
+
+// // Задача 1 сколько времени нужно на поиск элемента под номером [3]
+// Console.WriteLine(array[3]);
+// // Сложность алгоритма O(1)
+
+// ЗАдача 2 Узнать сумму массива. 
+// Сколько операций необходимо чтобы узнать сумму массива
+// [4,5,3,1,2]
